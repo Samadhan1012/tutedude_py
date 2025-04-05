d975b155454c102c06793598d26949cc6801798b
Samadhan1012 <31381459+Samadhan1012@users.noreply.github.com>
2025-04-05 15:44:15 +0530

    Assignment 1

---
+++ Task1.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: {multiplication}")
+print(f"Division: {division}")
