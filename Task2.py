f79e603c77167bdeae2257cc096a798003851411
Samadhan1012 <31381459+Samadhan1012@users.noreply.github.com>
2025-04-17 00:18:42 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,4 +1,10 @@
-fname = input("Enter your first name: ")
-lname = input("Enter your last name: ")
+list = []
+five_elements = []
+for i in range(1,11):
+    list.append(i)
 
-print(f"Hello, {fname} {lname}! Welcome to the Python program.")
+print(f"Orignal list: {list}")
+five_elements = list[:5]
+print(f"Extracted first five elements: {five_elements}")
+five_elements.reverse()
+print(f"Reversed extracted elements: {five_elements}")
